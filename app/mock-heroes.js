c07c9dca6609e6fd457d27bc78f636979d10c9e1
Andreas Danek <a.danek@live.com>
2016-08-01 23:01:25 +0200

    Configuration

--- app/mock-heroes.js
+++ app/mock-heroes.js
@@ -9,6 +9,6 @@ exports.HEROES = [
     { id: 17, name: 'Dynama' },
     { id: 18, name: 'Dr IQ' },
     { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 20, name: 'Tornados' }
 ];
 //# sourceMappingURL=mock-heroes.js.map
